28981bea53844185a2bdaa0a1b54e7fec442c7a2
kagaba-etienne <kagabaetienne04@gmail.com>
2025-09-14 20:43:27 +0200

    Console 0.0.1

--- console.py
+++ console.py
@@ -1,1 +1,37 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+"""
+Command interpreter for the AirBnB clone project.
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Command interpreter class for the AirBnB clone project.
+    """
+    
+    prompt = "(hbnb) "
+    
+    def emptyline(self):
+        """
+        Do nothing when an empty line is entered.
+        """
+        return False
+    
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
+        """
+        return True
+    
+    def do_EOF(self, arg):
+        """
+        EOF command to exit the program
+        """
+        print()
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
